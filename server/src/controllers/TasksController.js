fea3bcef5a6688815fc610b911d26f034ac8cf90
Bappy-Hossain <bh78764@gmail.com>
2024-11-03 20:52:15 +0900

    create delete update tasks

---
+++ server/src/controllers/TasksController.js
@@ -0,0 +1,44 @@
+const TasksModel = require("../models/TasksModel");
+
+//Create Task
+exports.createTask = (req, res) => {
+  let reqBody = req.body;
+  reqBody.email = req.headers["email"];
+  TasksModel.create(reqBody, (err, data) => {
+    if (err) {
+      res.status(400).json({ status: "Fail", data: err });
+    } else {
+      res.status(200).json({ status: "success", data: data });
+    }
+  });
+};
+
+//Delete Task
+exports.deleteTask = (req, res) => {
+  let id = req.params.id;
+  let Query = { _id: id };
+
+  TasksModel.remove(Query, (err) => {
+    if (err) {
+      res.status(400).json({ status: "Fail", data: err });
+    } else {
+      res.status(200).json({ status: "success" });
+    }
+  });
+};
+
+//Update Task
+exports.updateTaskStatus = (req, res) => {
+  let id = req.params.id;
+  let status = req.params.status;
+  let Query = { _id: id };
+  let reqBody = { status: status };
+
+  TasksModel.updateOne(Query, reqBody, (err, data) => {
+    if (err) {
+      res.status(400).json({ status: "Fail", data: err });
+    } else {
+      res.status(200).json({ status: "success", data: data });
+    }
+  });
+};
--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UsersController = require("../controllers/UsersController");
+const TasksController = require("../controllers/TasksController");
 const AuthVerifyMiddleware = require("../middleware/AuthVerifyMiddleware");
 
+//User api
 router.post("/registration", UsersController.registration);
 router.post("/login", UsersController.login);
 router.post(
@@ -10,5 +12,14 @@ router.post(
   AuthVerifyMiddleware,
   UsersController.profileUpdate
 );
+
+//Task api
+router.post("/createTask", AuthVerifyMiddleware, TasksController.createTask);
+router.get(
+  "/updateTaskStatus/:id/:status",
+  AuthVerifyMiddleware,
+  TasksController.updateTaskStatus
+);
+router.get("/deleteTask/:id", AuthVerifyMiddleware, TasksController.deleteTask);
 
 module.exports = router;
