d2928c2e14abdd3b4bae5be3fbf35bc646cae21d
Bappy-Hossain <bh78764@gmail.com>
2024-11-08 01:20:10 +0900

    Page Routing set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardPage from "./pages/Dashboard-Page";
+import CreatePage from "./pages/Create-Page";
+import NewPage from "./pages/New-Page";
+import ProgressPage from "./pages/Progress-Page";
+import CompletedPage from "./pages/Completed-Page";
+import CanceledPage from "./pages/Canceled-Page";
+import ProfilePage from "./pages/Profile-Page";
+import LoginPage from "./pages/Login-Page";
+import RegistrationPage from "./pages/Registration-Page";
+import Forgetpass from "./pages/Forgetpass-Page";
+import Page404 from "./pages/Page-404";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<DashboardPage />} />
+          <Route exact path="/Create" element={<CreatePage />} />
+          <Route exact path="/All" element={<NewPage />} />
+          <Route exact path="/Progress" element={<ProgressPage />} />
+          <Route exact path="/Completed" element={<CompletedPage />} />
+          <Route exact path="/Canceled" element={<CanceledPage />} />
+          <Route exact path="/Profile" element={<ProfilePage />} />
+          <Route exact path="/Login" element={<LoginPage />} />
+          <Route exact path="/Registration" element={<RegistrationPage />} />
+          <Route exact path="/Forgetpass" element={<Forgetpass />} />
+          <Route exact path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
-}
+};
 
 export default App;
--- client/src/pages/Dashboard-Page.jsx
+++ client/src/pages/Dashboard-Page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>Dashboard-Page</div>;
+  return <div>This is Dashboard page...</div>;
 };
 
 export default DashboardPage;
